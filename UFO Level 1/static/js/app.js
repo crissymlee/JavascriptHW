b99584de095d2287db03b9acfe08f8df97f101a2
crissymlee <66131888+crissymlee@users.noreply.github.com>
2020-10-20 22:28:44 -0500

    UPDATE2

--- UFO Level 1/static/js/app.js
+++ UFO Level 1/static/js/app.js
@@ -11,25 +11,25 @@ var process = d3.select("#filter-btn");
 
    d3.select("tbody").html("");
 
-// Get the value property of the input element
+// GET DATA
     var date_time = d3.select("#datetime").property("value");
      console.log(date_time);
 
     var filteredData = tableData.filter(record => record.datetime === date_time);
      console.log(filteredData)
 
-//  Display the filtered dataset
+//  FILTERED DATA
     filteredData.forEach(displayTable);
 
 });
 
-// Define a function to append table based on reports data
+// APPEND TABLE
 function displayTable(data) {
     var tbody = d3.select("tbody");
 
     var row = tbody.append("tr");
 
-// for each key value pair in an object
+// APPEND FOR EACH KEY PAIR
     Object.entries(data).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
